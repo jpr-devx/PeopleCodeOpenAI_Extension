c9c8c4276b2d79673d1630c68fb404d2c75f1b39
jpr-devx <john.riordan.dev@outlook.com>
2024-11-17 18:49:43 -0800

    Removed unused import

--- src/main/java/ai/peoplecode/OpenAIConversation.java
+++ src/main/java/ai/peoplecode/OpenAIConversation.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
